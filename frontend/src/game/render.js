8b861c155baa60ddc297046dbe0c961590a564c6
johndahllucifer-del <johndahllucifer@gmail.com>
2026-08-08 01:24:35 +0300

    Update render.js

--- frontend/src/game/render.js
+++ frontend/src/game/render.js
@@ -241,7 +241,7 @@ if (equippedBow.id === "emerald") {
   };
 
   // Ortadaki ana zümrüt
-drawCrystal(27, 0, 11, 0);
+drawCrystal(30, 0, 9, 0);
 
   // Üst kol kristalleri
   drawCrystal(8, -24, 7, -0.35);
